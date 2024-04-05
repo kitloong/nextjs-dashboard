030e06a74560716583b6db25e98c8979a20f571a
Kit Loong <kitloong1008@gmail.com>
2024-04-05 14:54:02 +0800

    Add translation

--- src/app/ui/dashboard/Sidebar/SidebarNav.tsx
+++ src/app/ui/dashboard/Sidebar/SidebarNav.tsx
@@ -18,6 +18,7 @@ import React, { PropsWithChildren } from 'react'
 import { Badge } from 'react-bootstrap'
 import SidebarNavGroup from '@/app/ui/dashboard/Sidebar/SidebarNavGroup'
 import SidebarNavItem from '@/app/ui/dashboard/Sidebar/SidebarNavItem'
+import { getDictionary } from '@/locales/dictionary'
 
 const SidebarNavTitle = (props: PropsWithChildren) => {
   const { children } = props
@@ -27,88 +28,89 @@ const SidebarNavTitle = (props: PropsWithChildren) => {
   )
 }
 
-export default function SidebarNav() {
+export default async function SidebarNav() {
+  const dict = await getDictionary()
   return (
     <ul className="list-unstyled">
       <SidebarNavItem icon={faGauge} href="/">
-        Dashboard
+        {dict.sidebar.items.dashboard}
         <small className="ms-auto"><Badge bg="info" className="ms-auto">NEW</Badge></small>
       </SidebarNavItem>
       <SidebarNavItem icon={faCode} href="/pokemons">
-        Sample
+        {dict.sidebar.items.sample}
         <small className="ms-auto"><Badge bg="danger" className="ms-auto">DEMO</Badge></small>
       </SidebarNavItem>
       <SidebarNavTitle>Theme</SidebarNavTitle>
-      <SidebarNavItem icon={faDroplet} href="#">Colors</SidebarNavItem>
-      <SidebarNavItem icon={faPencil} href="#">Typography</SidebarNavItem>
+      <SidebarNavItem icon={faDroplet} href="#">{dict.sidebar.items.colors}</SidebarNavItem>
+      <SidebarNavItem icon={faPencil} href="#">{dict.sidebar.items.typography}</SidebarNavItem>
       <SidebarNavTitle>Components</SidebarNavTitle>
 
-      <SidebarNavGroup toggleIcon={faPuzzlePiece} toggleText="Base">
-        <SidebarNavItem href="#">Accordion</SidebarNavItem>
-        <SidebarNavItem href="#">Breadcrumb</SidebarNavItem>
-        <SidebarNavItem href="#">Cards</SidebarNavItem>
-        <SidebarNavItem href="#">Carousel</SidebarNavItem>
-        <SidebarNavItem href="#">Collapse</SidebarNavItem>
-        <SidebarNavItem href="#">List group</SidebarNavItem>
-        <SidebarNavItem href="#">Navs</SidebarNavItem>
-        <SidebarNavItem href="#">Pagination</SidebarNavItem>
-        <SidebarNavItem href="#">Popovers</SidebarNavItem>
-        <SidebarNavItem href="#">Progress</SidebarNavItem>
-        <SidebarNavItem href="#">Scrollspy</SidebarNavItem>
-        <SidebarNavItem href="#">Spinners</SidebarNavItem>
-        <SidebarNavItem href="#">Tables</SidebarNavItem>
-        <SidebarNavItem href="#">Tabs</SidebarNavItem>
-        <SidebarNavItem href="#">Tooltips</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faPuzzlePiece} toggleText={dict.sidebar.items.base}>
+        <SidebarNavItem href="#">{dict.sidebar.items.accordion}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.breadcrumb}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.cards}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.carousel}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.collapse}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.listGroup}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.navs}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.pagination}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.popovers}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.progress}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.scrollspy}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.spinners}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.tables}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.tabs}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.tooltips}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavGroup toggleIcon={faLocationArrow} toggleText="Buttons">
-        <SidebarNavItem href="#">Buttons</SidebarNavItem>
-        <SidebarNavItem href="#">Buttons Group</SidebarNavItem>
-        <SidebarNavItem href="#">Dropdowns</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faLocationArrow} toggleText={dict.sidebar.items.buttons}>
+        <SidebarNavItem href="#">{dict.sidebar.items.buttons}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.buttonsGroup}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.dropdowns}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavItem icon={faChartPie} href="#">Charts</SidebarNavItem>
+      <SidebarNavItem icon={faChartPie} href="#">{dict.sidebar.items.charts}</SidebarNavItem>
 
-      <SidebarNavGroup toggleIcon={faFileLines} toggleText="Forms">
-        <SidebarNavItem href="#">Form Control</SidebarNavItem>
-        <SidebarNavItem href="#">Select</SidebarNavItem>
-        <SidebarNavItem href="#">Checks and radios</SidebarNavItem>
-        <SidebarNavItem href="#">Range</SidebarNavItem>
-        <SidebarNavItem href="#">Input group</SidebarNavItem>
-        <SidebarNavItem href="#">Floating labels</SidebarNavItem>
-        <SidebarNavItem href="#">Layout</SidebarNavItem>
-        <SidebarNavItem href="#">Validation</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faFileLines} toggleText={dict.sidebar.items.forms}>
+        <SidebarNavItem href="#">{dict.sidebar.items.formControl}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.select}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.checksAndRadios}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.range}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.inputGroup}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.floatingLabels}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.layout}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.validation}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavGroup toggleIcon={faStar} toggleText="Icons">
-        <SidebarNavItem href="#">CoreUI Icons</SidebarNavItem>
-        <SidebarNavItem href="#">CoreUI Icons - Brand</SidebarNavItem>
-        <SidebarNavItem href="#">CoreUI Icons - Flag</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faStar} toggleText={dict.sidebar.items.icons}>
+        <SidebarNavItem href="#">{dict.sidebar.items.coreUIIcons}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.coreUIIconsBrand}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.coreUIIconsFlag}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavGroup toggleIcon={faBell} toggleText="Notifications">
-        <SidebarNavItem href="#">Alerts</SidebarNavItem>
-        <SidebarNavItem href="#">Badge</SidebarNavItem>
-        <SidebarNavItem href="#">Modals</SidebarNavItem>
-        <SidebarNavItem href="#">Toasts</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faBell} toggleText={dict.sidebar.items.notifications}>
+        <SidebarNavItem href="#">{dict.sidebar.items.alerts}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.badge}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.modals}</SidebarNavItem>
+        <SidebarNavItem href="#">{dict.sidebar.items.toasts}</SidebarNavItem>
       </SidebarNavGroup>
 
       <SidebarNavItem icon={faCalculator} href="#">
-        Widgets
+        {dict.sidebar.items.widgets}
         <small className="ms-auto"><Badge bg="info">NEW</Badge></small>
       </SidebarNavItem>
 
-      <SidebarNavTitle>Extras</SidebarNavTitle>
+      <SidebarNavTitle>{dict.sidebar.items.extras}</SidebarNavTitle>
 
-      <SidebarNavGroup toggleIcon={faStar} toggleText="Pages">
-        <SidebarNavItem icon={faRightToBracket} href="login">Login</SidebarNavItem>
-        <SidebarNavItem icon={faAddressCard} href="register">Register</SidebarNavItem>
-        <SidebarNavItem icon={faBug} href="#">Error 404</SidebarNavItem>
-        <SidebarNavItem icon={faBug} href="#">Error 500</SidebarNavItem>
+      <SidebarNavGroup toggleIcon={faStar} toggleText={dict.sidebar.items.pages}>
+        <SidebarNavItem icon={faRightToBracket} href="login">{dict.sidebar.items.login}</SidebarNavItem>
+        <SidebarNavItem icon={faAddressCard} href="register">{dict.sidebar.items.register}</SidebarNavItem>
+        <SidebarNavItem icon={faBug} href="#">{dict.sidebar.items.error404}</SidebarNavItem>
+        <SidebarNavItem icon={faBug} href="#">{dict.sidebar.items.error500}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavItem icon={faFileLines} href="#">Docs</SidebarNavItem>
-      <SidebarNavItem icon={faLayerGroup} href="https://coreui.io/pro/">Try CoreUI PRO</SidebarNavItem>
+      <SidebarNavItem icon={faFileLines} href="#">{dict.sidebar.items.docs}</SidebarNavItem>
+      <SidebarNavItem icon={faLayerGroup} href="https://coreui.io/pro/">{dict.sidebar.items.tryCoreUIPRO}</SidebarNavItem>
     </ul>
   )
 }
